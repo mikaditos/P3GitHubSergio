971d9645bf3cb44a8d2dbb720dcf925470f95c0b
mikaditos <160121560+mikaditos@users.noreply.github.com>
2024-03-07 11:09:29 +0100

    suma de numeros

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Aplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int numero1 = 3;
+        int numero2 = 5;
+        int suma = numero1 + numero2;
+        System.out.println("El resultado de la suma es: " + suma);
+    }
+}
