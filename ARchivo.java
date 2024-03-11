637cc9e4a8d683a0c806fd8c97c7f28e6aa63994
joseaandres12 <joseandresmolinamejia1@gmail.com>
2024-03-11 12:13:59 +0100

    Archivo
    ejercicio 11

---
+++ ARchivo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ARchivo {
+
+    public static void main(String[] args) {
+        System.out.println("Esto es de sergio");
+    }
+}
